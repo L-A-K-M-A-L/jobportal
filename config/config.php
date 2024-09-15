5c7d2e25d961b16e4a07aabd61a88f5e6ea7c422
lakmalprasanna <lakmalprasannakd@gmail.com>
2024-09-15 11:22:37 +0530

    Creating config file and database connection

---
+++ config/config.php
@@ -0,0 +1,25 @@
+<?php
+// this php page dedicated for connection with mysql databse
+
+   try{
+    $host = "localhost";
+    $dbname = "jobboard";
+    $user = "root";
+    $pass = "";
+
+
+    // PDO -> PHP data objects
+    // DSn -> Data Source Name
+    $conn = new PDO("mysql:host=$host;dbname=$dbname" , $user, $pass);
+    $conn ->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Data Base Connected successfully!";
+   }catch(PDOException $e){
+    echo $e->getMessage();
+   }
+    
+   // if($conn == true ){
+    //     echo"Database Connect Successfully1";
+    // }else{
+    //     echo "Error! Database Connection failed1";
+    // }
+?>
